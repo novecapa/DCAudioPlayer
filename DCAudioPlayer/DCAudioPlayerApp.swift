856925ece5d4c195ca3c5d7794d4236d47741998
Josep Cerdá Penadés <josep@daapcoders.com>
2025-01-18 20:40:12 +0100

    Initial commit

---
+++ DCAudioPlayer/DCAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCAudioPlayerApp.swift
+//  DCAudioPlayer
+//
+//  Created by Josep Cerdá Penadés on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DCAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DCAudioPlayerTests/DCAudioPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DCAudioPlayerTests.swift
+//  DCAudioPlayerTests
+//
+//  Created by Josep Cerdá Penadés on 18/1/25.
+//
+
+import Testing
+@testable import DCAudioPlayer
+
+struct DCAudioPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
